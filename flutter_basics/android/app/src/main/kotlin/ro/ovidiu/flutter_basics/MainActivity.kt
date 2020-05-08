8d279733d3560d6b854ddad19300d7b2a79b52b0
Ovidiu Cristurean <ovidiu.cristurean@endava.com>
2020-05-08 17:32:30 +0300

    Initialize template project

---
+++ flutter_basics/android/app/src/main/kotlin/ro/ovidiu/flutter_basics/MainActivity.kt
@@ -0,0 +1,12 @@
+package ro.ovidiu.flutter_basics
+
+import android.os.Bundle
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GeneratedPluginRegistrant.registerWith(this)
+    }
+}
